65190572e4c8eef682cec3266fae700e4b7d9f4d
KethakiKarunathilake <144051948+KethakiKarunathilake@users.noreply.github.com>
2024-04-18 15:40:54 +0530

    debugging in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ import WithPWA from "next-pwa";
 
 const withPWA = WithPWA({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  //disable: process.env.NODE_ENV === "development",
   register: true,
   scope: "/",
   sw: "service-worker.js",
